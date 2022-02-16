3727ea0317efeccbd0b9200fd45214a37f37bd6b
Johannes Naylor <jonaylor89@gmail.com>
2022-02-15 23:12:40 -0500

    add tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,15 @@
 import pytest
 
-# TODO
+from cyberpunk.config import CyberpunkConfig
+
+@pytest.fixture
+def default_config():
+    # db_fd, db_path = tempfile.mkstemp()
+    config = CyberpunkConfig() 
+
+    yield config
+
+def test_defaults(default_config):
+    port = default_config.port 
+
+    assert port == 5000
--- tests/test_local_storage.py
+++ tests/test_local_storage.py
@@ -1,3 +1,14 @@
 import pytest
 
-# TODO
+from cyberpunk.storage import LocalStorage
+
+@pytest.fixture
+def local_storage():
+    storage = LocalStorage() 
+
+    yield storage
+
+def test_local(local_storage):
+    base_dir = local_storage.base_dir 
+
+    assert base_dir == 'testdata/'
