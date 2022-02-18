8bd4ba6dca1dc0e348a519d5f36385c55843be41
Johannes Naylor <jonaylor89@gmail.com>
2022-02-17 23:29:36 -0500

    formatting

--- cyberpunk/processing.py
+++ cyberpunk/processing.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, List, Tuple
 
 from pydub import AudioSegment
 
@@ -22,6 +22,12 @@ def process_args(base_filename: str, args: Dict) -> Tuple[str, str]:
         "concat": Concat(),
     }
 
+    supported_formats: List[str] = [
+        "mp3",
+        "wav",
+        "flac",
+    ]
+
     # Create Audio Segment
     audio_segment: AudioSegment = get_storage().get_segment(base_filename)
 
@@ -37,13 +43,20 @@ def process_args(base_filename: str, args: Dict) -> Tuple[str, str]:
             inputs: Dict[str, Any] = transformation.parse_input_from_str(v)
             audio_segment = transformation.process(audio_segment, inputs)
 
+    # TODO this can be significantly better
+    file_format = "mp3"
+    if "format" in args.keys():
+        file_format = (
+            args["format"] if args["format"] in supported_formats else "mp3"
+        )
+
     processed_filename = get_storage().save_segment(
         base_filename,
         audio_segment,
     )
 
     # Return Filename and Audio Type
-    return processed_filename, "audio/mp3"
+    return processed_filename, f"audio/{file_format}"
 
 
 def parse_query(filename: str, args: Dict) -> Dict:
