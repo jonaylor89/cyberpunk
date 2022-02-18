92ea2807e4d0d094269740e21d61111f19c0c62b
Johannes Naylor <jonaylor89@gmail.com>
2022-02-18 00:25:09 -0500

    s3

--- cyberpunk/storage/s3.py
+++ cyberpunk/storage/s3.py
@@ -3,15 +3,29 @@ import logging
 import boto3
 from pydub import AudioSegment
 
+from cyberpunk.config import get_config
+
 
 class S3Storage:
     def __init__(self):
-        self.s3 = boto3.resource("s3")
+        config = get_config()
+
+        self.s3 = boto3.client("s3")
+        self.s3_storage_bucket = config.s3_storage.s3_storage_bucket
+        self.s3_storage_base_dir = config.s3_storage.s3_storage_base_dir
 
     def get_segment(self, key: str) -> AudioSegment:
         logging.info(f"pulling key from aws s3: {key}")
 
-        return AudioSegment.empty()
+        self.s3.download_file(
+            self.s3_storage_bucket,
+            f"{self.s3_storage_base_dir}{key}",
+            f"testdata/{key}",
+        )
+
+        segment = AudioSegment.from_file(f"testdata/{key}")
+
+        return segment
 
     def save_segment(
         self,
